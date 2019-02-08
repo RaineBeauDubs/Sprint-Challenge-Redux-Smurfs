828921d096057246af354060c92d533020eea4eb
Raine Wallace <rainebeaudubs@gmail.com>
2019-02-08 11:45:12 -0500

    Getting started on my reducer, original welcome message displaying to the screen again instead of an error.

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,6 +1,22 @@
+import {
+  FETCH_SMURFS_START,
+  FETCH_SMURFS_SUCCESS,
+  FETCH_SMURFS_ERROR,
+  POST_SMURFS_START,
+  POST_SMURFS_SUCCESS,
+  POST_SMURFS_ERROR
+} from '../actions/index';
 /*
   Be sure to import in all of the action types from `../actions`
 */
+const initialState = {
+  smurfs: [],
+   fetchingSmurfs: false,
+   addingSmurf: false,
+   updatingSmurf: false,
+   deletingSmurf: false,
+   error: null
+}
 
 /*
  Your initial/default state for this project could *Although does not have to* look a lot like this
@@ -14,6 +30,13 @@
  }
 */
 
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
 /*
   You'll only need one smurf reducer for this project.
   Feel free to export it as a default and import as rootReducer. 
@@ -21,3 +44,5 @@
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
+
+export default reducer;
